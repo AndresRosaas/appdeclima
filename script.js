fafea56ae166913438b62bee732b731a231cd7f3
Andres Rosas <114107146+AndresRosaas@users.noreply.github.com>
2025-01-28 21:18:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
-const API_KEY = '5723a27a16155fd326150931e8184ce3'
+const API_KEY = 'apikey'
 const diffKelvin = 273.15
 
 document.getElementById('searchButton').addEventListener('click', ()=>{
